b569cdf521af6773c4699ba192b30c67d406c9bc
alexander <alexander.van1dalen@gmail.com>
2020-04-01 10:38:46 +0200

    first commit

---
+++ src/test/java/com/example/cdb/CdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
